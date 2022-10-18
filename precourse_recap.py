38c6172616d01350aa17077613a4b6c4b985378a
Olive-kaa <olive.kaa@gmail.com>
2022-10-18 09:11:24 +0100

    updated precourse file

--- precourse_recap.py
+++ precourse_recap.py
@@ -3,8 +3,4 @@ nr_puppies = 1 + 3
 
 def mistery(name, nr_puppies):
     print("Where is " + name + "and their " + nr_puppies + "puppies?")
-<<<<<<< HEAD
-=======
-
->>>>>>> a7d7d57 (updated precourse file)
 
